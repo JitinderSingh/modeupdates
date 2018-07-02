7327837f5b2d377c462ae558abf6c69acb17e6cd
Jitinder Singh <JitinderSingh@users.noreply.github.com>
2018-07-02 16:58:31 +0530

    Update Edit.ascx.designer.cs

--- Edit.ascx.designer.cs
+++ Edit.ascx.designer.cs
@@ -8,8 +8,6 @@
 //------------------------------------------------------------------------------
 
 namespace DotNetNuke.Modules.MyDNN {
-    
-    
     public partial class Edit {
         
         /// <summary>
